8bab31a2f0d1474dae6e31dc00ce9f9d3fe6194f
Alamin <alamin8740@gmail.com>
2022-07-29 20:55:10 +0600

    add 2 Component home and friends

--- Friend_List_CRUD-angular-UI/src/app/app.module.ts
+++ Friend_List_CRUD-angular-UI/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HomeComponent } from './home/home.component';
+import { FriendsComponent } from './friends/friends.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    FriendsComponent
   ],
   imports: [
     BrowserModule,
